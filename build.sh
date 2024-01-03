5a385d44a4420f4b4a5d29689bb72091e6217a28
Faraz Fallahi <fffaraz@gmail.com>
2024-01-03 00:35:02 -0800

    update

--- build.sh
+++ build.sh
@@ -8,10 +8,16 @@ syslinux boot.img
 mkdir mnt
 mount boot.img mnt
 
-cp ./myiso/bzImage mnt
-cp ./myiso/initramfs mnt
-cp ./myiso/memtest mnt
-cp ./myiso/isolinux/isolinux.cfg mnt/syslinux.cfg
+cp ./myiso/bzImage ./mnt
+cp ./myiso/initramfs ./mnt
+cp ./myiso/memtest ./mnt
+cp ./myiso/isolinux/isolinux.cfg ./mnt/syslinux.cfg
+cp /usr/lib/syslinux/modules/bios/libcom32.c32 ./mnt
+cp /usr/lib/syslinux/modules/bios/libutil.c32 ./mnt
+cp /usr/lib/syslinux/modules/bios/vesamenu.c32 ./mnt
 
 umount mnt
 rmdir mnt
+
+cp boot.img /output
+cp mydistro.iso /output
