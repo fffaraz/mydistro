53ebb693a67ae4bc91255bc62fee03e064770764
Faraz Fallahi <fffaraz@gmail.com>
2025-09-27 14:11:37 -0700

    1

--- v0/src/mk-boot-img.sh
+++ v0/src/mk-boot-img.sh
@@ -10,10 +10,10 @@ syslinux boot.img
 mkdir -p ./mnt
 mount boot.img ./mnt
 
-cp ./myiso/bzImage ./mnt
-cp ./myiso/initramfs ./mnt
-cp ./myiso/memtest ./mnt
-cp ./myiso/isolinux/isolinux.cfg ./mnt/syslinux.cfg
+cp ./isodir/bzImage ./mnt
+cp ./isodir/initramfs ./mnt
+cp ./isodir/memtest ./mnt
+cp ./isodir/isolinux/isolinux.cfg ./mnt/syslinux.cfg
 cp /usr/lib/syslinux/modules/bios/libcom32.c32 ./mnt
 cp /usr/lib/syslinux/modules/bios/libutil.c32 ./mnt
 cp /usr/lib/syslinux/modules/bios/vesamenu.c32 ./mnt
