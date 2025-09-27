0f1cd68d42a6fbfe88e9289da793a8bfe4394fe3
Faraz Fallahi <fffaraz@gmail.com>
2025-09-27 14:10:23 -0700

    1

--- v0/build.sh
+++ v0/build.sh
@@ -2,9 +2,8 @@
 set -exuo pipefail
 
 rm -rf ./output
-docker rm -f mydistro
-
 mkdir ./output
+docker rm -f mydistro
 
 # docker pull debian:sid-slim
 docker build -t mydistro .
@@ -13,9 +12,12 @@ docker build -t mydistro .
 # cp mydistro.iso /output
 
 docker run --privileged -d -it --name mydistro mydistro
+
 docker cp mydistro:/opt/mydistro/src/linux/arch/x86/boot/bzImage $(pwd)/output/bzImage
 docker cp mydistro:/opt/mydistro/myiso/initramfs $(pwd)/output/initramfs.cpio
 docker cp mydistro:/opt/mydistro/mydistro.iso $(pwd)/output/mydistro.iso
+
 docker exec -it mydistro /opt/mydistro/mk-boot-img.sh
 docker cp mydistro:/opt/mydistro/boot.img $(pwd)/output/boot.img
+
 docker rm -f mydistro
--- v0/src/mk-boot-img.sh
+++ v0/src/mk-boot-img.sh
@@ -7,8 +7,8 @@ mkfs -t fat boot.img
 
 syslinux boot.img
 
-mkdir mnt
-mount boot.img mnt
+mkdir -p ./mnt
+mount boot.img ./mnt
 
 cp ./myiso/bzImage ./mnt
 cp ./myiso/initramfs ./mnt
@@ -19,8 +19,8 @@ cp /usr/lib/syslinux/modules/bios/libutil.c32 ./mnt
 cp /usr/lib/syslinux/modules/bios/vesamenu.c32 ./mnt
 cp /usr/lib/syslinux/modules/bios/menu.c32 ./mnt
 
-umount mnt
-rmdir mnt
+umount ./mnt
+rmdir ./mnt
 
 # cp boot.img /output
 # cp mydistro.iso /output
