3dd3da62d658e4cd3439c5a7bb5be6a984b0afd4
Faraz Fallahi <fffaraz@gmail.com>
2025-09-26 22:42:09 -0700

    Update run.sh

--- v0/run.sh
+++ v0/run.sh
@@ -4,4 +4,4 @@ set -exuo pipefail
 qemu-system-x86_64 -cpu host -enable-kvm -smp 2 -m 256M -cdrom ./output/mydistro.iso -net nic,model=virtio -net user
 
 # qemu-system-x86_64 ./output/boot.img
-# qemu-system-x86_64 -m 512M -kernel ./output/bzImage -initrd ./output/initramfs.cpio -append "console=ttyS0" -nographic -enable-kvm
+# qemu-system-x86_64 -m 512M -kernel ./output/bzImage -initrd ./output/initramfs.cpio -append "console=ttyS0" -nographic
