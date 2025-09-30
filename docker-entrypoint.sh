4c9f48f66ac25eb91f04023f209f69cac0ab7157
Faraz Fallahi <fffaraz@gmail.com>
2025-09-30 02:15:35 -0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,9 +11,14 @@ set -exuo pipefail
 ./scripts/0010-curl.sh
 ./scripts/0011-nano.sh
 
-cp ./assets/init.sh /opt/mydistro/initramfs-dir/init
-cp ./assets/syslinux.cfg /opt/mydistro/iso-dir/isolinux/isolinux.cfg
+cp ./assets/init.sh ./initramfs-dir/init
+cp ./assets/syslinux.cfg ./iso-dir/isolinux/isolinux.cfg
 
 ./scripts/0012-initramfs.sh
 
+cp ./src/linux/arch/x86/boot/bzImage ./output/bzImage
+cp ./iso-dir/initramfs.cpio ./output/initramfs.cpio
+cp ./mydistro.iso ./output/mydistro.iso
+
 ./assets/mk-boot-img.sh
+cp ./boot.img ./output/boot.img
