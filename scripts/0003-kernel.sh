19547454f7692e48605c9b642b6d54cd6a1ff657
Faraz Fallahi <fffaraz@gmail.com>
2025-11-25 22:44:47 -0800

    Update 0003-kernel.sh

--- scripts/0003-kernel.sh
+++ scripts/0003-kernel.sh
@@ -4,24 +4,22 @@ set -x
 # compile linux kernel
 
 cd /opt/mydistro/src/linux
+
 make defconfig
 
 # make menuconfig
 
-sed -i 's/^CONFIG_CC_VERSION_TEXT=.*/CONFIG_CC_VERSION_TEXT="gcc (mydistro)"/' .config
+# sed -i 's/^CONFIG_CC_VERSION_TEXT=.*/CONFIG_CC_VERSION_TEXT="gcc (mydistro)"/' .config
 
-# ./scripts/config --enable IKCONFIG
-# ./scripts/config --enable IKCONFIG_PROC
-sed -i 's/# CONFIG_IKCONFIG is not set/CONFIG_IKCONFIG=y/' .config
-echo "CONFIG_IKCONFIG_PROC=y" >> .config
+./scripts/config --enable IKCONFIG
+./scripts/config --enable IKCONFIG_PROC
+./scripts/config --enable DRM_FBDEV_EMULATION # Enable legacy fbdev support for your modesetting driver
+./scripts/config --enable DRM_BOCHS # DRM Support for bochs dispi vga interface (qemu stdvga)
+./scripts/config --enable LOGO # Bootup logo
 
-# CONFIG_DRM_FBDEV_EMULATION: Enable legacy fbdev support for your modesetting driver
-# CONFIG_DRM_BOCHS: DRM Support for bochs dispi vga interface (qemu stdvga)
-# CONFIG_LOGO: Bootup logo
+# cp /opt/mydistro/assets/linux.config .config
 
-cp /opt/mydistro/assets/linux.config .config
-
-# make olddefconfig
+make olddefconfig
 
 make -j$(nproc)
 
