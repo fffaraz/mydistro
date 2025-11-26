c29a9f5e5f1b04aa49b2a054954f301dd99be12b
Faraz Fallahi <fffaraz@gmail.com>
2025-11-25 22:05:23 -0800

    Update 0003-kernel.sh

--- scripts/0003-kernel.sh
+++ scripts/0003-kernel.sh
@@ -11,6 +11,11 @@ make defconfig
 sed -i 's/^CONFIG_CC_VERSION_TEXT=.*/CONFIG_CC_VERSION_TEXT="gcc (mydistro)"/' .config
 sed -i 's/# CONFIG_IKCONFIG is not set/CONFIG_IKCONFIG=y/' .config
 echo "CONFIG_IKCONFIG_PROC=y" >> .config
+# CONFIG_DRM_FBDEV_EMULATION: Enable legacy fbdev support for your modesetting driver
+# CONFIG_DRM_BOCHS: DRM Support for bochs dispi vga interface (qemu stdvga)
+# CONFIG_LOGO: Bootup logo
+
+cp /opt/mydistro/assets/linux.config .config
 
 make -j$(nproc)
 
