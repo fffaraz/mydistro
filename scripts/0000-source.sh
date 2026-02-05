efa00da8673149fbd3dd69e803aea69e60aa20e4
Faraz Fallahi <fffaraz@gmail.com>
2026-02-04 23:04:34 -0800

    Update 0000-source.sh

--- scripts/0000-source.sh
+++ scripts/0000-source.sh
@@ -10,7 +10,7 @@ cd ./src
 git config --global advice.detachedHead false
 
 [ -d ./linux ] || git clone --depth 1 -b v6.16 https://github.com/torvalds/linux.git
-[ -d ./busybox ] || git clone --depth 1 https://github.com/mirror/busybox.git # https://git.busybox.net/busybox
+[ -d ./busybox ] || git clone --depth 1 -b 1_37_0 https://git.busybox.net/busybox # https://github.com/mirror/busybox.git
 [ -d ./syslinux ] || git clone --depth 1 https://salsa.debian.org/images-team/syslinux.git # git://repo.or.cz/syslinux.git
 [ -d ./memtest86plus ] || git clone --depth 1 https://github.com/memtest86plus/memtest86plus.git
 [ -d ./microwindows ] || git clone --depth 1 https://github.com/ghaerr/microwindows.git
@@ -21,7 +21,6 @@ git config --global advice.detachedHead false
 [ -d ./toybox ] || git clone --depth 1 https://github.com/landley/toybox.git
 [ -d ./coreutils ] || git clone --depth 1 https://github.com/coreutils/coreutils.git # git://git.savannah.gnu.org/coreutils.git
 [ -d ./wlroots ] || git clone --depth 1 -b 0.18.3 https://gitlab.freedesktop.org/wlroots/wlroots.git
-
 [ -d ./nano ] || git clone --depth 1 https://github.com/madnight/nano.git # https://git.savannah.gnu.org/git/nano.git
 [ -d ./gnulib ] || git clone https://github.com/coreutils/gnulib.git ./gnulib # git://git.sv.gnu.org/gnulib.git
 
