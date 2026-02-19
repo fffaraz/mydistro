49c4513b0cf8f2b81df4fc276f31811f4d50d1ea
Faraz Fallahi <fffaraz@gmail.com>
2026-02-18 23:25:39 -0800

    Update 1005-m4.sh

--- scripts/1005-m4.sh
+++ scripts/1005-m4.sh
@@ -6,7 +6,7 @@ cd ./src/m4
 rmdir ./gnulib
 ln -s ../gnulib ./gnulib
 
-./bootstrap
+./bootstrap --skip-po
 ./configure --prefix=/usr   \
             --host=$LFS_TGT \
             --build=$(build-aux/config.guess)
