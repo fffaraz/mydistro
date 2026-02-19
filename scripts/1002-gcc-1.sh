094044bac856101918bbc09d64ba4bf7bfee50d1
Faraz Fallahi <fffaraz@gmail.com>
2026-02-18 22:53:28 -0800

    Update 1002-gcc-1.sh

--- scripts/1002-gcc-1.sh
+++ scripts/1002-gcc-1.sh
@@ -41,4 +41,4 @@ make
 make install
 
 cd ..
-cat gcc/limitx.h gcc/glimits.h gcc/limity.h > `dirname $($LFS/tools/bin/$LFS_TGT-gcc -print-libgcc-file-name)`/include/limits.h
+cat gcc/limitx.h gcc/glimits.h gcc/limity.h > `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include/limits.h
