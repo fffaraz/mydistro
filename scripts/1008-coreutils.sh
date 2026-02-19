8b16af76f1233e30634b151d6c9ecee940ab776d
Faraz Fallahi <fffaraz@gmail.com>
2026-02-19 01:18:42 -0800

    Update 1008-coreutils.sh

--- scripts/1008-coreutils.sh
+++ scripts/1008-coreutils.sh
@@ -9,7 +9,7 @@ cd ./src/coreutils
             --build=$(build-aux/config.guess) \
             --enable-install-program=hostname \
             --enable-no-install-program=kill,uptime \
-            CFLAGS="-Wno-error=unterminated-string-initialization -Wno-error=cast-align -Wno-error=switch-enum -Wno-error=calloc-transposed-args"
+            CFLAGS="-Wno-error=unterminated-string-initialization -Wno-error=cast-align -Wno-error=switch-enum -Wno-error=calloc-transposed-args -Wno-error=maybe-uninitialized"
 
 make
 make DESTDIR=$LFS install
