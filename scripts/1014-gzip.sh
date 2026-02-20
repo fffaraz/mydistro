611ccb13c091e1909c6396b1c7e60e7ae7657491
Faraz Fallahi <fffaraz@gmail.com>
2026-02-19 22:56:48 -0800

    1

--- scripts/1014-gzip.sh
+++ scripts/1014-gzip.sh
@@ -6,6 +6,6 @@ cd ./src/gzip
 cp -r --reflink=auto ../gnulib ./gnulib-repo
 ./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib-repo
 
-./configure --prefix=/usr --host=$LFS_TGT
+./configure --prefix=/usr --host=$LFS_TGT CFLAGS="-Wno-error=zero-as-null-pointer-constant"
 make
 make DESTDIR=$LFS install
--- scripts/1015-make.sh
+++ scripts/1015-make.sh
@@ -3,6 +3,9 @@ set -exuo pipefail
 
 cd ./src/make
 
+cp -r --reflink=auto ../gnulib ./gnulib-repo
+./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib-repo
+
 ./configure --prefix=/usr   \
             --host=$LFS_TGT \
             --build=$(build-aux/config.guess)
