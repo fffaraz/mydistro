ce018dfdf3395864099e71f68199f47d72377371
Faraz Fallahi <fffaraz@gmail.com>
2026-02-19 23:57:29 -0800

    Update 1018-tar.sh

--- scripts/1018-tar.sh
+++ scripts/1018-tar.sh
@@ -5,15 +5,33 @@ cd ./src/tar
 
 rmdir paxutils || true
 rm paxutils || true
-ln -s ../paxutils paxutils
+ln -s ../paxutils ./paxutils
 
 cp -r --reflink=auto ../gnulib ./gnulib-repo
 ./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib-repo
 
+echo '#include <sys/ioctl.h>' > lib/system-ioctl.h
+
+# Fix type mismatch: paxutils uses idx_t, tar uses size_t/ssize_t
+sed -i 's/void write_error_details (char const \*name, size_t status, size_t size)/void write_error_details (char const *name, idx_t status, idx_t size)/' src/common.h
+sed -i 's/_Noreturn void write_fatal_details (char const \*name, ssize_t status, size_t size)/_Noreturn void write_fatal_details (char const *name, idx_t status, idx_t size)/' src/common.h
+sed -i 's/write_fatal_details (char const \*name, ssize_t status, size_t size)/write_fatal_details (char const *name, idx_t status, idx_t size)/' src/buffer.c
+sed -i '/#include "common.h"/a #include <ctype.h>' src/transform.c
+
+# Define legacy gnulib macros removed in newer versions
+sed -i '/^#include "tar.h"/a \
+#ifndef ISDIGIT\
+# define ISDIGIT(c) ((unsigned) (c) - '\''0'\'' <= 9)\
+#endif\
+#ifndef ISODIGIT\
+# define ISODIGIT(c) ((unsigned) (c) - '\''0'\'' <= 7)\
+#endif' src/common.h
+
 ./configure --prefix=/usr   \
             --host=$LFS_TGT \
             --build=$(build-aux/config.guess) \
-            CFLAGS="-Wno-error=missing-variable-declarations -Wno-error=zero-as-null-pointer-constant"
+            --disable-nls \
+            CFLAGS="-Wno-error"
 
 make
 make DESTDIR=$LFS install
