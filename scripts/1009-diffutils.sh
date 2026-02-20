60f233c5a48c4ec17bc61a55ccd2204d03e7f6db
Faraz Fallahi <fffaraz@gmail.com>
2026-02-19 21:00:30 -0800

    Update 1009-diffutils.sh

--- scripts/1009-diffutils.sh
+++ scripts/1009-diffutils.sh
@@ -3,9 +3,9 @@ set -exuo pipefail
 
 cd ./src/diffutils
 
-cp -r --reflink=auto ../gnulib ./gnulib
+cp -r --reflink=auto ../gnulib ./gnulib-repo
 
-./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib
+./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib-repo
 ./configure --prefix=/usr   \
             --host=$LFS_TGT \
             gl_cv_func_strcasecmp_works=y \
