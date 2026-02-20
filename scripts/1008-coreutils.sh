75a7b57e77fddd17aa26e58d1c264560aae0f7be
Faraz Fallahi <fffaraz@gmail.com>
2026-02-19 20:59:31 -0800

    Update 1008-coreutils.sh

--- scripts/1008-coreutils.sh
+++ scripts/1008-coreutils.sh
@@ -5,6 +5,8 @@ cd ./src/coreutils
 
 cp -r --reflink=auto ../gnulib ./gnulib-repo
 
+rm ./gl/top/maint.mk.diff
+
 git config --global --add safe.directory $(pwd)
 
 ./bootstrap --skip-po --gnulib-srcdir=./gnulib-repo
@@ -18,7 +20,7 @@ git config --global --add safe.directory $(pwd)
 make
 make DESTDIR=$LFS install
 
-mv -v $LFS/usr/bin/chroot              $LFS/usr/sbin
+# mv -v $LFS/usr/bin/chroot $LFS/usr/sbin
 mkdir -pv $LFS/usr/share/man/man8
 mv -v $LFS/usr/share/man/man1/chroot.1 $LFS/usr/share/man/man8/chroot.8
 sed -i 's/"1"/"8"/'                    $LFS/usr/share/man/man8/chroot.8
