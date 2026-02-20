4a1702c4a31a27f396f7b6ee9ce1a1f4fc3b91b7
Faraz Fallahi <fffaraz@gmail.com>
2026-02-19 22:54:27 -0800

    1

--- scripts/1012-gawk.sh
+++ scripts/1012-gawk.sh
@@ -3,6 +3,9 @@ set -exuo pipefail
 
 cd ./src/gawk
 
+ln -sf "$(which aclocal)" /usr/local/bin/aclocal-1.16
+ln -sf "$(which automake)" /usr/local/bin/automake-1.16
+
 sed -i 's/extras//' Makefile.in
 
 ./configure --prefix=/usr   \
--- scripts/1013-grep.sh
+++ scripts/1013-grep.sh
@@ -3,9 +3,13 @@ set -exuo pipefail
 
 cd ./src/grep
 
+cp -r --reflink=auto ../gnulib ./gnulib-repo
+./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib-repo
+
 ./configure --prefix=/usr   \
             --host=$LFS_TGT \
-            --build=$(./build-aux/config.guess)
+            --build=$(./build-aux/config.guess) \
+            CFLAGS="-Wno-error=zero-as-null-pointer-constant"
 
 make
 make DESTDIR=$LFS install
