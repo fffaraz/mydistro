7929eff422f1b9dab7de1a72145e969486d83f84
Faraz Fallahi <fffaraz@gmail.com>
2026-02-22 00:39:26 -0800

    Update 027-bison.sh

--- scripts1/027-bison.sh
+++ scripts1/027-bison.sh
@@ -8,6 +8,8 @@ cp -r --reflink=auto ../gnulib ./gnulib-repo
 
 ./configure --prefix=/usr \
             --host=$LFS_TGT \
+            --disable-nls \
             --docdir=/usr/share/doc/bison-3.8.2
+
 make
 make DESTDIR=$LFS install
