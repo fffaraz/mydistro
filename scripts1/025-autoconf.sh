eeac8defc175cabe0d6966afae161f52157e2a10
Faraz Fallahi <fffaraz@gmail.com>
2026-02-21 18:37:34 -0800

    Update 025-autoconf.sh

--- scripts1/025-autoconf.sh
+++ scripts1/025-autoconf.sh
@@ -3,7 +3,9 @@ set -exuo pipefail
 
 cd ./src/autoconf
 
-./bootstrap
-./configure --prefix=/usr --host=$LFS_TGT
+autoreconf -vif
+./configure --prefix=/usr   \
+            --host=$LFS_TGT \
+            --build=$(build-aux/config.guess)
 make
 make DESTDIR=$LFS install
