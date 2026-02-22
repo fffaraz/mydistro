3e5791af7d3ed68beeade80eb1ccb7673439f68e
Faraz Fallahi <fffaraz@gmail.com>
2026-02-21 20:18:09 -0800

    Update 004-gettext.sh

--- scripts2/004-gettext.sh
+++ scripts2/004-gettext.sh
@@ -6,6 +6,8 @@ cd ./src/gettext
 rmdir ./gnulib
 ln -s ../gnulib gnulib
 
+sed -i "/emit += 'AM_CFLAGS %s.*am_set_or_augment/i\\        emit += 'AM_CFLAGS =\\\\n'" gnulib-repo/pygnulib/GLEmiter.py
+
 ./autogen.sh
 ./configure --disable-shared
 make
