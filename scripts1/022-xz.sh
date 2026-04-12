4821ef96090b2e68c8c2415926710f7768fc181e
Faraz Fallahi <fffaraz@gmail.com>
2026-04-11 22:20:19 -0700

    1

--- scripts1/022-xz.sh
+++ scripts1/022-xz.sh
@@ -2,16 +2,20 @@
 set -exuo pipefail
 
 cd ./src
-[ -d xz ] || (tar xf xz-*.tar.* && mv xz-*/ xz)
-cd ./xz
-
-autoreconf -vif
+if [ ! -d xz ]; then
+  tar xf xz-*.tar.*
+  mv xz-*/ xz
+  cd ./xz
+else
+  cd ./xz
+  autoreconf -vif
+fi
 
 ./configure --prefix=/usr                     \
             --host=$LFS_TGT                   \
             --build=$(build-aux/config.guess) \
             --disable-static                  \
-            --docdir=/usr/share/doc/xz-5.8.1
+            --docdir=/usr/share/doc/xz-5.8.2
 
 make
 make DESTDIR=$LFS install
