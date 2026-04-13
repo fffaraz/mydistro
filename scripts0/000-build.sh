789f3f685106bd73c95dd5e164bf8e417423df2c
Faraz Fallahi <fffaraz@gmail.com>
2026-04-12 18:27:08 -0700

    1

--- scripts0/000-build.sh
+++ scripts0/000-build.sh
@@ -11,15 +11,15 @@ export CFLAGS="${COMMON_FLAGS}"
 export CXXFLAGS="${COMMON_FLAGS}"
 export MAKEFLAGS=-j$(nproc)
 
-./scripts/0001-kernel.sh
-./scripts/0002-initramfs.sh
-./scripts/0004-busybox.sh
-./scripts/0005-syslinux.sh
-./scripts/0006-memtest86.sh
-./scripts/0007-microwindows.sh
-./scripts/0008-dropbear.sh
-./scripts/0010-curl.sh
-./scripts/0011-nano.sh
-./scripts/0012-initramfs.sh
-./scripts/0013-mkisofs.sh
-./scripts/0014-mk-boot-img.sh
+./scripts/001-kernel.sh
+./scripts/002-initramfs.sh
+./scripts/004-busybox.sh
+./scripts/005-syslinux.sh
+./scripts/006-memtest86.sh
+./scripts/007-microwindows.sh
+./scripts/008-dropbear.sh
+./scripts/010-curl.sh
+./scripts/011-nano.sh
+./scripts/012-initramfs.sh
+./scripts/013-mkisofs.sh
+./scripts/014-mk-boot-img.sh
