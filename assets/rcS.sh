6c5e1a18a60c569037a1de5993840a9b55ef4068
Faraz Fallahi <fffaraz@gmail.com>
2026-04-12 21:53:16 -0700

    Update rcS.sh

--- assets/rcS.sh
+++ assets/rcS.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 set -x
 
+/bin/mount -t proc proc /proc
+/bin/mount -t sysfs sysfs /sys
+/bin/mount -t devtmpfs dev /dev
+/bin/mount -t tmpfs -o mode=1777,strictatime tmpfs /tmp
+
+/bin/hostname myhostname
+/sbin/ifconfig lo 127.0.0.1
+
+export HOME=/root
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin
 export TERM=xterm-256color
+export PS1='\w\$ '
+
+set +x
+echo -e "\nBoot took $(cut -d' ' -f1 /proc/uptime) seconds\n"
