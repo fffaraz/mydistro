3526eec84e9f296347ad9a76e1129e8834994410
Faraz Fallahi <fffaraz@gmail.com>
2026-04-12 17:35:22 -0700

    Update 008-export.sh

--- scripts2/008-export.sh
+++ scripts2/008-export.sh
@@ -6,4 +6,4 @@ find /usr/{lib,libexec} -name \*.la -delete
 rm -rf /tools
 
 rm -f /opt/mydistro/output/stage3.tar.gz
-tar --exclude=/opt/mydistro --exclude=/proc --exclude=/sys --exclude=/dev -czf /opt/mydistro/output/stage3.tar.gz /
+tar --exclude=/opt/mydistro --exclude=/proc --exclude=/sys --exclude=/dev --exclude=/.dockerenv -czf /opt/mydistro/output/stage3.tar.gz /
