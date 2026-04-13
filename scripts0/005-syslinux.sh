a34a4f360939525cece7151a37eacf4689d37694
Faraz Fallahi <fffaraz@gmail.com>
2026-04-12 19:06:35 -0700

    Update 005-syslinux.sh

--- scripts0/005-syslinux.sh
+++ scripts0/005-syslinux.sh
@@ -5,6 +5,8 @@ cd ./src/syslinux
 
 # apply patches
 for f in debian/patches/*.patch; do patch -p1 < $f; done; unset f
-sed -i '/#include <stdbool.h>/a #include <stdio.h>' ./com32/lib/syslinux/debug.c
 
-CFLAGS="" LDFLAGS="" DATE=not-too-long make OPTFLAGS="-O3" bios
+sed -i '/#include <stdbool.h>/a #include <stdio.h>' ./com32/lib/syslinux/debug.c
+sed -i 's/lmalloc\.o/lmalloc.o calloc.o/' ./mk/lib.mk
+
+CFLAGS="" CXXFLAGS="" LDFLAGS="" DATE=not-too-long make OPTFLAGS="-O3 -Wno-error" bios
