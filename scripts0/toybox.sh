f29342f1eb4d101991adffafd42f1c3aead423cf
Faraz Fallahi <fffaraz@gmail.com>
2026-04-12 21:43:03 -0700

    1

--- scripts0/toybox.sh
+++ scripts0/toybox.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+set -exuo pipefail
 
 # compile toybox
 cd ./src/toybox
 
-make defconfig
+make defconfig || true
 # make menuconfig
+
+sed -i 's/# CONFIG_INIT is not set/CONFIG_INIT=y/' .config
+sed -i 's/# CONFIG_GETTY is not set/CONFIG_GETTY=y/' .config
+sed -i 's/# CONFIG_VI is not set/CONFIG_VI=y/' .config
+make oldconfig
 
 make
 
