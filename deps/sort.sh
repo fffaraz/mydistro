9589a37b5f4f32aa3cf33ffbcdee3cb846f3f7c1
Faraz Fallahi <fffaraz@gmail.com>
2026-04-12 23:33:53 -0700

    Update sort.sh

--- deps/sort.sh
+++ deps/sort.sh
@@ -4,11 +4,11 @@
 DIR="$(dirname "$0")"
 
 for FILE in "$DIR"/sources-*.conf; do
-  echo "Sorting $FILE"
-  {
-    # Print header (comments and blank lines at the top)
-    sed -n '/^[^#]/q;p' "$FILE"
-    # Sort the non-comment, non-empty lines
-    grep -v '^#' "$FILE" | grep -v '^$' | sort
-  } > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
+	echo "Sorting $FILE"
+	{
+		# Print header (comments and blank lines at the top)
+		sed -n '/^[^#]/q;p' "$FILE"
+		# Sort the non-comment, non-empty lines
+		grep -v '^#' "$FILE" | grep -v '^$' | sort
+	} >"$FILE.tmp" && mv "$FILE.tmp" "$FILE"
 done
