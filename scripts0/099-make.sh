29f1deb0a01b990d90fc4b71b1931417cd3df94e
Faraz Fallahi <fffaraz@gmail.com>
2026-04-13 20:10:56 -0700

    1

---
+++ scripts0/099-make.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -exuo pipefail
+
+cd ./src/make
+cp -r --reflink=auto ../gnulib ./gnulib-repo
+./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib-repo
+sed -i '1i #define streq(s1, s2) (strcmp(s1, s2) == 0)' lib/concat-filename.c lib/findprog-in.c
+
+./configure --disable-nls
+make
+make install DESTDIR=/opt/mydistro/initramfs-dir
