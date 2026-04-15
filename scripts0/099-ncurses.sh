127a15f0467ec4adade49f8727a63f9a892b988e
Faraz Fallahi <fffaraz@gmail.com>
2026-04-14 23:23:57 -0700

    Update 099-ncurses.sh

--- scripts0/099-ncurses.sh
+++ scripts0/099-ncurses.sh
@@ -3,7 +3,18 @@ set -exuo pipefail
 
 cd ./src/ncurses
 
-./configure --prefix=/usr
+./configure \
+	--prefix=/usr \
+	--mandir=/usr/share/man \
+	--with-shared \
+	--with-termlib \
+	--without-normal \
+	--with-cxx-shared \
+	--without-debug \
+	--without-ada \
+	--disable-stripping \
+	AWK=gawk
 
 make
 make install DESTDIR=$INITRAMFS_DIR
+ln -svf libncursesw.so "$INITRAMFS_DIR/usr/lib/libncurses.so"
