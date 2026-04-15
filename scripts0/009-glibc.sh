fb8f0f01bb5694706d5f59e7f9f3c918a6d51dcd
Faraz Fallahi <fffaraz@gmail.com>
2026-04-14 19:43:47 -0700

    Update 009-glibc.sh

--- scripts0/009-glibc.sh
+++ scripts0/009-glibc.sh
@@ -3,14 +3,10 @@ set -exuo pipefail
 
 cd ./src/glibc
 
-ln -s ../mpfr mpfr
-ln -s ../gmp gmp
-ln -s ../mpc mpc
-
 mkdir build
 cd build
 
-../configure --disable-nls --enable-languages=c,c++
+../configure --prefix=/usr
 
 make
 make install DESTDIR=/opt/mydistro/initramfs-dir
