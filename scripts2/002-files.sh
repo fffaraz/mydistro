cf5382e6dad31520ed1b92ad5d81f46f88c42458
Faraz Fallahi <fffaraz@gmail.com>
2026-04-23 23:01:17 -0700

    1

--- scripts2/002-files.sh
+++ scripts2/002-files.sh
@@ -4,5 +4,5 @@ set -exuo pipefail
 ln -sv /proc/self/mounts /etc/mtab
 
 cp ./assets/etc/hosts /etc/hosts
-cp ./assets/etc-passwd /etc/passwd
-cp ./assets/etc-group /etc/group
+cp ./assets/etc/passwd /etc/passwd
+cp ./assets/etc/group /etc/group
