3f75567f8793c7c1b1e4b11888fa1c893483756e
Faraz Fallahi <fffaraz@gmail.com>
2026-04-24 00:26:00 -0700

    Update 012-glibc.sh

--- scripts2/012-glibc.sh
+++ scripts2/012-glibc.sh
@@ -45,11 +45,11 @@ tar -xf ../../tzdata2025c.tar.gz
 ZONEINFO=/usr/share/zoneinfo
 mkdir -pv $ZONEINFO/{posix,right}
 
-for tz in etcetera southamerica northamerica europe africa antarctica  \
-          asia australasia backward; do
-    zic -L /dev/null   -d $ZONEINFO       ${tz}
-    zic -L /dev/null   -d $ZONEINFO/posix ${tz}
-    zic -L leapseconds -d $ZONEINFO/right ${tz}
+for tz in etcetera southamerica northamerica europe africa antarctica \
+	asia australasia backward; do
+	zic -L /dev/null -d $ZONEINFO ${tz}
+	zic -L /dev/null -d $ZONEINFO/posix ${tz}
+	zic -L leapseconds -d $ZONEINFO/right ${tz}
 done
 
 cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO
