7ff624785e35dce8fee42bcc2a9284a0d269952d
Faraz Fallahi <fffaraz@gmail.com>
2026-04-23 22:54:20 -0700

    1

--- scripts1/000-build.sh
+++ scripts1/000-build.sh
@@ -43,10 +43,9 @@ export CXXFLAGS="${COMMON_FLAGS}"
 ./scripts/023-binutils-2.sh
 ./scripts/024-gcc-2.sh
 
-exit 0
+# ./scripts/025-autoconf.sh
+# ./scripts/026-automake.sh
+# ./scripts/027-bison.sh
+# ./scripts/028-gperf.sh
 
-./scripts/025-autoconf.sh
-./scripts/026-automake.sh
-./scripts/027-bison.sh
-./scripts/028-gperf.sh
 ./scripts/029-export.sh
