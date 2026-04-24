34fb8c2e5c4ea0c5ec89cafde74f15ebaa874f7b
Faraz Fallahi <fffaraz@gmail.com>
2026-04-23 22:52:49 -0700

    Update 000-build.sh

--- scripts2/000-build.sh
+++ scripts2/000-build.sh
@@ -6,10 +6,11 @@ cp -r --reflink=auto ./src-ro ./src
 export HOME=/root
 export TERM=xterm-256color
 export PS1='\u@\h \W]\$ '
+export PATH=/usr/bin:/usr/sbin
 export MAKEFLAGS=-j$(nproc)
 export TESTSUITEFLAGS=-j$(nproc)
 
-COMMON_FLAGS="-O2 -pipe -march=native -Wno-error"
+COMMON_FLAGS="-O3 -pipe -march=native -Wno-error"
 export CFLAGS="${COMMON_FLAGS}"
 export CXXFLAGS="${COMMON_FLAGS}"
 
