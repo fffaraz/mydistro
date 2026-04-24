8ae3d9ff6c73f0c14cbe889e175d961907102b2c
Faraz Fallahi <fffaraz@gmail.com>
2026-04-23 23:15:26 -0700

    Update 002-files.sh

--- scripts2/002-files.sh
+++ scripts2/002-files.sh
@@ -9,5 +9,5 @@ cp ./assets/etc/group /etc/group
 
 touch /var/log/{btmp,lastlog,faillog,wtmp}
 chgrp -v utmp /var/log/lastlog
-chmod -v 664  /var/log/lastlog
-chmod -v 600  /var/log/btmp
+chmod -v 664 /var/log/lastlog
+chmod -v 600 /var/log/btmp
