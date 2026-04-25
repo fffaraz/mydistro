766b5a1c7fac0b56d018e3fe9cf20e943fe785eb
Faraz Fallahi <fffaraz@gmail.com>
2026-04-25 16:12:43 -0700

    Update download-src.sh

--- scripts/download-src.sh
+++ scripts/download-src.sh
@@ -84,7 +84,7 @@ while IFS= read -r line || [[ -n "$line" ]]; do
 		fi
 		echo "==> Cloning $name from $repo @ $branch"
 		rm -rf "$name.tmp"
-		git -c advice.detachedHead=false clone --depth=1 --branch "$branch" "$repo" "$name.tmp"
+		git -c advice.detachedHead=false clone --depth=1 --recurse-submodules --branch "$branch" "$repo" "$name.tmp"
 		mv "$name.tmp" "$name"
 	else
 		echo "Error: unknown type '$type' on line: $line" >&2
