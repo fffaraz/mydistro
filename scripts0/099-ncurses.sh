263d3b9cdf04e4ca2adc4f20c4f25845e8983263
Faraz Fallahi <fffaraz@gmail.com>
2026-04-25 15:20:53 -0700

    Update 099-ncurses.sh

--- scripts0/099-ncurses.sh
+++ scripts0/099-ncurses.sh
@@ -5,7 +5,7 @@ cd ./src/ncurses
 
 ./configure \
 	--prefix=/usr \
-	--mandir=/usr/share/man \
+	--mandir=/tmp/ncurses-man \
 	--with-shared \
 	--with-termlib \
 	--without-normal \
@@ -17,9 +17,3 @@ cd ./src/ncurses
 
 make
 make install DESTDIR=$INITRAMFS_DIR
-
-ln -svf libncursesw.so "$INITRAMFS_DIR/usr/lib/libncurses.so"
-ln -svf libtinfow.so.6 "$INITRAMFS_DIR/usr/lib/libtinfo.so.6"
-ln -svf libtinfow.so "$INITRAMFS_DIR/usr/lib/libtinfo.so"
-ln -svf /usr/lib/libtinfow.so.6 "$INITRAMFS_DIR/lib64/libtinfo.so.6"
-ln -svf /usr/lib/libtinfow.so "$INITRAMFS_DIR/lib64/libtinfo.so"
