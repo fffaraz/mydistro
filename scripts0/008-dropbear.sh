c12dadb94eaca56fc5d1ac8062e3ffb33660cd71
Faraz Fallahi <fffaraz@gmail.com>
2026-04-25 16:01:08 -0700

    1

--- scripts0/008-dropbear.sh
+++ scripts0/008-dropbear.sh
@@ -3,7 +3,7 @@ set -exuo pipefail
 
 cd ./src/dropbear
 
-./configure  --prefix=/usr --mandir=/tmp/dropbear-man
+./configure --prefix=/usr --mandir=/tmp/dropbear-man
 
 make
 make install DESTDIR=$INITRAMFS_DIR
