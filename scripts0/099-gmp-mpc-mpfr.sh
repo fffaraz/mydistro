2a2abd34166227edfb46e081b0c99774c283bbe7
Faraz Fallahi <fffaraz@gmail.com>
2026-04-25 13:35:32 -0700

    Update 099-gmp-mpc-mpfr.sh

--- scripts0/099-gmp-mpc-mpfr.sh
+++ scripts0/099-gmp-mpc-mpfr.sh
@@ -3,15 +3,19 @@ set -exuo pipefail
 
 cd ./src
 
-cd ./gmp
-# no need to configure gpm
-cd ..
+[ -d ./gmp ] || (tar xf gmp-*.tar.* && mv gmp-*/ gmp)
 
-cd ./mpc
-git config --global --add safe.directory $(pwd)
-autoreconf -vif
-cd ..
+[ -d ./mpc ] || (tar xf mpc-*.tar.* && mv mpc-*/ mpc)
+if [ -d ./mpc/.git ]; then
+	cd ./mpc
+	git config --global --add safe.directory $(pwd)
+	autoreconf -vif
+	cd ..
+fi
 
-cd ./mpfr
-./autogen.sh
-cd ..
+[ -d ./mpfr ] || (tar xf mpfr-*.tar.* && mv mpfr-*/ mpfr)
+if [ -d ./mpfr/.git ]; then
+	cd ./mpfr
+	./autogen.sh
+	cd ..
+fi
