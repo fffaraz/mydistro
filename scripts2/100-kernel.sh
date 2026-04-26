b3c38424340cc65581ef7405b5450345a7921624
Faraz Fallahi <fffaraz@gmail.com>
2026-04-26 02:26:25 -0700

    Update 100-kernel.sh

--- scripts2/100-kernel.sh
+++ scripts2/100-kernel.sh
@@ -10,11 +10,61 @@ make mrproper
 
 make defconfig
 
-./scripts/config --enable IKCONFIG
-./scripts/config --enable IKCONFIG_PROC
+# General setup
+./scripts/config --disable WERROR
+./scripts/config --enable  PSI
+./scripts/config --disable PSI_DEFAULT_DISABLED
+./scripts/config --disable IKHEADERS
+./scripts/config --enable  CGROUPS
+./scripts/config --enable  MEMCG
+./scripts/config --enable  CGROUP_SCHED
+./scripts/config --disable RT_GROUP_SCHED
+./scripts/config --disable EXPERT
+
+# Processor type and features
+./scripts/config --enable  RELOCATABLE
+./scripts/config --enable  RANDOMIZE_BASE
+
+# General architecture-dependent options
+./scripts/config --enable  STACKPROTECTOR
+./scripts/config --enable  STACKPROTECTOR_STRONG
+
+# Networking
+./scripts/config --enable  NET
+./scripts/config --enable  INET
+./scripts/config --enable  IPV6
+
+# Device Drivers — generic + firmware + framebuffer + DRM
+./scripts/config --disable UEVENT_HELPER
+./scripts/config --enable  DEVTMPFS
+./scripts/config --enable  DEVTMPFS_MOUNT
+./scripts/config --enable  FW_LOADER
+./scripts/config --disable FW_LOADER_USER_HELPER
+./scripts/config --enable  DMIID
+./scripts/config --enable  SYSFB_SIMPLEFB
+./scripts/config --enable  DRM
+./scripts/config --enable  DRM_PANIC
+./scripts/config --set-str DRM_PANIC_SCREEN kmsg
+./scripts/config --enable  DRM_FBDEV_EMULATION
+./scripts/config --enable  DRM_SIMPLEDRM
+./scripts/config --enable  FRAMEBUFFER_CONSOLE
+
+# File systems
+./scripts/config --enable  INOTIFY_USER
+./scripts/config --enable  TMPFS
+./scripts/config --enable  TMPFS_POSIX_ACL
+
+# 64-bit additions (x86_64)
+./scripts/config --enable  X86_X2APIC
+./scripts/config --enable  PCI
+./scripts/config --enable  PCI_MSI
+./scripts/config --enable  IOMMU_SUPPORT
+./scripts/config --enable  IRQ_REMAP
+
+# NVMe block device (needed if root is on /dev/nvme*)
+./scripts/config --enable  BLK_DEV_NVME
 
 make olddefconfig
-
 
 make
 make modules_install
