e4273a25e951d25b28b330164ebcc8282d928dcb
Faraz Fallahi <fffaraz@gmail.com>
2026-04-26 01:35:33 -0700

    Update 036-shadow.sh

--- scripts2/036-shadow.sh
+++ scripts2/036-shadow.sh
@@ -9,6 +9,7 @@ cd ./shadow
 sed -i 's/groups$(EXEEXT) //' src/Makefile.in
 find man -name Makefile.in -exec sed -i 's/groups\.1 / /'   {} \;
 find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \;
+find man -name Makefile.in -exec sed -i 's/passwd\.5 / /'   {} \;
 
 sed -e 's:#ENCRYPT_METHOD DES:ENCRYPT_METHOD YESCRYPT:' \
     -e 's:/var/spool/mail:/var/mail:' \
