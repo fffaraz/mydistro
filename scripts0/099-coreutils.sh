5137f26ebda33924f0ae7c0127252d6a0f7bd661
Faraz Fallahi <fffaraz@gmail.com>
2026-05-02 21:03:31 -0700

    Update 099-coreutils.sh

--- scripts0/099-coreutils.sh
+++ scripts0/099-coreutils.sh
@@ -7,7 +7,7 @@ cp -r --reflink=auto ../gnulib ./gnulib-repo
 rm -f ./gl/top/maint.mk.diff
 ./bootstrap --skip-po --gnulib-srcdir=./gnulib-repo
 
-./configure --prefix=/usr --enable-install-program=hostname --enable-no-install-program=kill,uptime --disable-nls
+FORCE_UNSAFE_CONFIGURE=1 ./configure --prefix=/usr --enable-install-program=hostname --enable-no-install-program=kill,uptime --disable-nls
 
 make
 make install DESTDIR=$INITRAMFS_DIR
