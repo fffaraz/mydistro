348a8a286dfd78da065608224d7314f9215b4771
Faraz Fallahi <fffaraz@gmail.com>
2026-05-02 21:37:04 -0700

    Update 099-coreutils.sh

--- scripts0/099-coreutils.sh
+++ scripts0/099-coreutils.sh
@@ -3,13 +3,11 @@ set -exuo pipefail
 
 cd ./src/coreutils
 
-cp -r --reflink=auto ../gnulib ./gnulib-repo
-rm -f ./gl/top/maint.mk.diff
-./bootstrap --skip-po --gnulib-srcdir=./gnulib-repo
+./bootstrap --skip-po 
 
 FORCE_UNSAFE_CONFIGURE=1 ./configure --prefix=/usr --enable-install-program=hostname --enable-no-install-program=kill,uptime --disable-nls
 
-grep -q '^#include <string.h>' lib/mbbuf.h || sed -i '/^#include "idx.h"/a #include <string.h>' lib/mbbuf.h
+# grep -q '^#include <string.h>' lib/mbbuf.h || sed -i '/^#include "idx.h"/a #include <string.h>' lib/mbbuf.h
 
 make
 make install DESTDIR=$INITRAMFS_DIR
