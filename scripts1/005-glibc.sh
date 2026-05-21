3c7f25de9bd8c36c17c5b447c4e31ee3b9105966
Faraz Fallahi <fffaraz@gmail.com>
2026-05-21 00:43:03 -0700

    Update 005-glibc.sh

--- scripts1/005-glibc.sh
+++ scripts1/005-glibc.sh
@@ -39,11 +39,11 @@ sed '/RTLDLIST=/s@/usr@@g' -i $LFS/usr/bin/ldd
 echo 'int main(){}' | $LFS_TGT-gcc -x c - -v -Wl,--verbose &>dummy.log
 readelf -l a.out | grep ': /lib'
 
-grep -E -o "$LFS/lib.*/S?crt[1in].*succeeded" dummy.log
-grep -B3 "^ $LFS/usr/include" dummy.log
-grep 'SEARCH.*/usr/lib' dummy.log | sed 's|; |\n|g'
-grep "/lib.*/libc.so.6 " dummy.log
-grep found dummy.log
+grep -E -o "$LFS/lib.*/S?crt[1in].*succeeded" dummy.log || true
+grep -B3 "^ $LFS/usr/include" dummy.log || true
+grep 'SEARCH.*/usr/lib' dummy.log | sed 's|; |\n|g' || true
+grep "/lib.*/libc.so.6 " dummy.log || true
+grep found dummy.log || true
 
 rm -v a.out dummy.log
 
