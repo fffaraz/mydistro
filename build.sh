36cb1e97df979010b356cf62242e1069b8edda36
Faraz Fallahi <fffaraz@gmail.com>
2026-05-21 02:27:50 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,6 +24,7 @@ if [ "$BUILD_LFS" = "1" ]; then
 	./scripts/build-lfs-1.sh   # first stage
 	./scripts/build-lfs-2.sh 2 # second stage
 	./scripts/build-lfs-2.sh 3 # third stage
+	./scripts/build-lfs-2.sh 4 # fourth stage
 else
 	./scripts/build1.sh # first pass
 	./scripts/build2.sh # second pass
