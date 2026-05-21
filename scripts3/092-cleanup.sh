8f6e168dfad28caf1c36cde066b71595f1b034af
Faraz Fallahi <fffaraz@gmail.com>
2026-05-21 01:32:55 -0700

    1

--- scripts3/092-cleanup.sh
+++ scripts3/092-cleanup.sh
@@ -8,3 +8,12 @@ find /usr/lib /usr/libexec -name \*.la -delete
 find /usr -depth -name $(uname -m)-lfs-linux-gnu\* | xargs rm -rf
 
 # userdel -r tester || true
+
+rm -f ./output/stage4.tar.gz
+tar \
+	--exclude=/opt/mydistro \
+	--exclude=/proc \
+	--exclude=/sys \
+	--exclude=/dev \
+	--exclude=/.dockerenv \
+	-czf ./output/stage4.tar.gz /
---
+++ scripts4/000-build.sh
@@ -0,0 +1,11 @@
+
+./scripts/093-network.sh
+./scripts/094-clock.sh
+./scripts/095-locale.sh
+./scripts/096-inputrc.sh
+./scripts/097-etcshells.sh
+./scripts/098-systemd-custom.sh
+
+./scripts/099-fstab.sh
+./scripts/100-kernel.sh
+./scripts/101-grub.sh
