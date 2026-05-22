c59bfd98e55166c785fdb7cb68cbcc8a9db6f79c
Faraz Fallahi <fffaraz@gmail.com>
2026-05-22 00:54:28 -0700

    Update 099-patch.sh

--- scripts0/099-patch.sh
+++ scripts0/099-patch.sh
@@ -2,8 +2,8 @@
 set -exuo pipefail
 
 cd ./src/patch
-cp -r --reflink=auto ../gnulib ./gnulib-repo
-./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib-repo
+
+./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib
 
 ./configure --prefix=/usr
 
