daf033c01aeb1807599d0dd0af97c933c0eb9110
Faraz Fallahi <fffaraz@gmail.com>
2026-05-21 23:43:07 -0700

    Update 009-glibc.sh

--- scripts0/009-glibc.sh
+++ scripts0/009-glibc.sh
@@ -3,12 +3,14 @@ set -exuo pipefail
 
 cd ./src/glibc
 
+# Work around missing redefinition guards in glibc 2.42's sys/mount.h:
+# it pulls in <linux/mount.h> (which now defines OPEN_TREE_CLONE et al.)
+# and then unconditionally redefines the same macros, which -Werror rejects.
+sed -i -E 's@^(#define (FSMOUNT_CLOEXEC|FSOPEN_CLOEXEC|OPEN_TREE_CLONE|OPEN_TREE_CLOEXEC|MOVE_MOUNT_[A-Z_]+|FSPICK_[A-Z_]+)\b)@#undef \2\n\1@' \
+	sysdeps/unix/sysv/linux/sys/mount.h
+
 mkdir build
 cd build
-
-sed -i -E '
-  s|^(#define (FSMOUNT_CLOEXEC|FSOPEN_CLOEXEC|OPEN_TREE_CLONE|OPEN_TREE_CLOEXEC|MOVE_MOUNT_[A-Z_]+|FSPICK_[A-Z_]+)\b)|#undef \2\n\1|
-' sysdeps/unix/sysv/linux/sys/mount.h
 
 ../configure --prefix=/usr
 
