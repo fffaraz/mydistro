e8a8ba16b26d92c85118e03f4ef1335f6b440bfa
Faraz Fallahi <fffaraz@gmail.com>
2026-05-21 20:37:00 -0700

    Update 003-export.sh

--- scripts5/003-export.sh
+++ scripts5/003-export.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+set -exuo pipefail
 
+rm -f ./output/stage6.tar.gz
+tar \
+	--exclude=/opt/mydistro \
+	--exclude=/proc \
+	--exclude=/sys \
+	--exclude=/dev \
+	--exclude=/.dockerenv \
+	-czf ./output/stage6.tar.gz /
