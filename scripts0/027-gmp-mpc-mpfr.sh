e79d21b99b0203cfdfeecfbb85a8b7a3fb537518
Faraz Fallahi <fffaraz@gmail.com>
2026-05-22 14:49:16 -0700

    Update 027-gmp-mpc-mpfr.sh

--- scripts0/027-gmp-mpc-mpfr.sh
+++ scripts0/027-gmp-mpc-mpfr.sh
@@ -8,28 +8,24 @@ tar xf gmp-*.tar.*
 mv gmp-*/ gmp
 
 # Build and install gmp (shared lib needed at runtime by coreutils' expr/factor).
-# gmp 6.3.0's configure-time "long long reliability test" relies on signed
-# left-shift overflow wrapping; newer gcc treats that as UB and miscompiles
-# the test, so configure rejects every ABI. -fwrapv forces wrap semantics.
-# --enable-fat builds a CPU-agnostic libgmp (no -march=native lock-in).
 cd gmp
-CFLAGS="-O2 -fwrapv" ./configure --prefix=/usr --enable-shared --enable-fat
-make
-make install DESTDIR=$INITRAMFS_DIR
+# ./configure --prefix=/usr --enable-shared --enable-fat
+# make
+# make install DESTDIR=$INITRAMFS_DIR
 cd ..
 
 # Build and install mpfr (depends on gmp).
 cd mpfr
 ./autogen.sh
-./configure --prefix=/usr --enable-shared
-make
-make install DESTDIR=$INITRAMFS_DIR
+# ./configure --prefix=/usr --enable-shared
+# make
+# make install DESTDIR=$INITRAMFS_DIR
 cd ..
 
 # Build and install mpc (depends on gmp + mpfr).
 cd mpc
 autoreconf -vif
-./configure --prefix=/usr --enable-shared
-make
-make install DESTDIR=$INITRAMFS_DIR
+# ./configure --prefix=/usr --enable-shared
+# make
+# make install DESTDIR=$INITRAMFS_DIR
 cd ..
