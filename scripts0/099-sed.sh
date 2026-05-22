e855ceac9b5fc9db09473737cf6ce1073049ff7e
Faraz Fallahi <fffaraz@gmail.com>
2026-05-22 00:54:34 -0700

    sed AM_CFLAGS

--- scripts0/099-sed.sh
+++ scripts0/099-sed.sh
@@ -2,9 +2,8 @@
 set -exuo pipefail
 
 cd ./src/sed
-cp -r --reflink=auto ../gnulib ./gnulib-repo
-sed -i "/emit += 'AM_CFLAGS %s.*am_set_or_augment/i\\        emit += 'AM_CFLAGS =\\\\n'" gnulib-repo/pygnulib/GLEmiter.py
-./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib-repo
+
+./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib
 
 ./configure --prefix=/usr --disable-nls
 
