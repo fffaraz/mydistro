41ea09d6402b83646f00ea264150ac99641af7ba
Faraz Fallahi <fffaraz@gmail.com>
2026-05-21 23:37:32 -0700

    Update 009-glibc.sh

--- scripts0/009-glibc.sh
+++ scripts0/009-glibc.sh
@@ -6,6 +6,10 @@ cd ./src/glibc
 mkdir build
 cd build
 
+sed -i -E '
+  s|^(#define (FSMOUNT_CLOEXEC|FSOPEN_CLOEXEC|OPEN_TREE_CLONE|OPEN_TREE_CLOEXEC|MOVE_MOUNT_[A-Z_]+|FSPICK_[A-Z_]+)\b)|#undef \2\n\1|
+' sysdeps/unix/sysv/linux/sys/mount.h
+
 ../configure --prefix=/usr
 
 make
