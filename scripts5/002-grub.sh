a7999b6b62f12c70db81535db8d64d6142032267
Faraz Fallahi <fffaraz@gmail.com>
2026-05-21 21:09:28 -0700

    Update 002-grub.sh

--- scripts5/002-grub.sh
+++ scripts5/002-grub.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -exuo pipefail
 
+rm -f ./output/boot.img
 dd if=/dev/zero of=./output/boot.img bs=1M count=2048
 sync
 mkfs -t ext4 ./output/boot.img
 sync
 
-grub-install ./output/boot.img
+grub-install --target i386-pc ./output/boot.img
 sync
 
 mkdir -p ./mnt
