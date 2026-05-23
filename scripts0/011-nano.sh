a5ef161e292be3fda2b37494304672b3a0aa9838
Faraz Fallahi <fffaraz@gmail.com>
2026-05-23 02:24:51 -0700

    Update 011-nano.sh

--- scripts0/011-nano.sh
+++ scripts0/011-nano.sh
@@ -7,8 +7,8 @@ cp -r --reflink=auto ../gnulib ./gnulib
 
 ./autogen.sh
 ./configure --prefix=/usr --enable-utf8 --enable-year2038 --disable-nls --docdir=/tmp/nano-doc --mandir=/tmp/nano-man --infodir=/tmp/nano-info
-make
-make install DESTDIR=$INITRAMFS_DIR
+make MAKEINFO=true
+make install MAKEINFO=true DESTDIR=$INITRAMFS_DIR
 
 # install terminfo entries so ncurses can initialize the terminal
 mkdir -p $INITRAMFS_DIR/usr/share/terminfo/x
