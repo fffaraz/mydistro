453ea9c1a743400eeb238626b8a0dc2bf20dedf8
Faraz Fallahi <fffaraz@gmail.com>
2026-05-23 20:33:09 -0700

    Update 099-bison.sh

--- scripts0/099-bison.sh
+++ scripts0/099-bison.sh
@@ -3,6 +3,11 @@ set -exuo pipefail
 
 cd ./src/bison
 
+# git-version-gen needs either a working `git describe` or .tarball-version.
+# Pass 2 has neither (no git binary in initramfs; shallow clone .git survives
+# the overlay but git itself doesn't), so seed the version explicitly.
+echo 3.8.2 >.tarball-version
+
 ./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib
 
 # sed -i '/\/\* directory \*\/ NULL,/a\ NULL,' src/output.c src/print-xml.c
