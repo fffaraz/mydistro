b199783e074790a99d2e25e349e6fdaa015c945c
Faraz Fallahi <fffaraz@gmail.com>
2026-05-24 02:16:45 -0700

    Update 099-groff.sh

--- scripts0/099-groff.sh
+++ scripts0/099-groff.sh
@@ -6,5 +6,12 @@ cd ./src/groff
 ./bootstrap
 
 PAGE=letter ./configure --prefix=/usr
+
+# xpmtoppm (netpbm) isn't in the distro yet, so bypass the rule that
+# regenerates this example doc image from doc/gnu.xpm.
+mkdir -p doc
+: >doc/gnu.eps
+touch -d '2099-01-01' doc/gnu.eps
+
 make
 make install DESTDIR=$INITRAMFS_DIR
