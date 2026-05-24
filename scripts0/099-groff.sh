a5e4663a584be126970dea78b1add47669c5cce5
Faraz Fallahi <fffaraz@gmail.com>
2026-05-24 11:51:54 -0700

    Update 099-groff.sh

--- scripts0/099-groff.sh
+++ scripts0/099-groff.sh
@@ -8,9 +8,17 @@ cd ./src/groff
 PAGE=letter ./configure --prefix=/usr
 
 # xpmtoppm (netpbm) isn't in the distro yet, so bypass the rule that
-# regenerates this example doc image from doc/gnu.xpm.
+# regenerates this example doc image from doc/gnu.xpm. PSPIC rejects an
+# empty file, so drop in a minimal valid EPS placeholder instead.
 mkdir -p doc
-: >doc/gnu.eps
+cat >doc/gnu.eps <<'EOF'
+%!PS-Adobe-3.0 EPSF-3.0
+%%BoundingBox: 0 0 1 1
+%%EndComments
+%%Page: 1 1
+showpage
+%%EOF
+EOF
 touch -d '2099-01-01' doc/gnu.eps
 
 make
