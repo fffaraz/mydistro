4f79f33dc1d5cfe3a1178bfbed7b82d0f314ff34
Faraz Fallahi <fffaraz@gmail.com>
2026-05-25 15:18:05 -0700

    Update 038-wget.sh

--- scripts0/038-wget.sh
+++ scripts0/038-wget.sh
@@ -2,6 +2,13 @@
 set -exuo pipefail
 
 cd ./src/wget
+
+# Stub out AX_CODE_COVERAGE (from autoconf-archive, which we don't ship).
+# aclocal will pick this up via -I m4 and expand the macro to nothing.
+cat >m4/ax_code_coverage.m4 <<'EOF'
+AC_DEFUN([AX_CODE_COVERAGE], [])
+EOF
+
 ./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib
 
 ./configure \
