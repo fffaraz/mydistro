1e2624c997f72da236a2521c4ccd4ac1617ffe42
Faraz Fallahi <fffaraz@gmail.com>
2026-05-24 17:01:42 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,16 +11,20 @@ unset GTK_PATH
 if [[ "${1:-}" == "--cli" ]]; then
 	echo "To exit QEMU when running in a terminal without a graphical interface: Ctrl+A -> X"
 	qemu-system-x86_64 -cpu host -enable-kvm -smp 2 -m 4G -kernel ./output/bzImage -initrd ./output/initramfs.cpio.gz -append "console=ttyS0" -nographic
+
 elif [[ "${1:-}" == "--iso" ]]; then
 	qemu-system-x86_64 -cpu host -enable-kvm -smp 2 -m 4G -cdrom ./output/mydistro.iso -net nic,model=virtio -net user
 	# qemu-system-x86_64 -cpu host -enable-kvm -smp 2 -m 4G -drive format=raw,media=cdrom,readonly=on,file=./output/mydistro.iso
+
 elif [[ "${1:-}" == "--img" ]]; then
 	# qemu-system-x86_64 -cpu host -enable-kvm -smp 2 -m 4G ./output/boot.img
 	qemu-system-x86_64 -cpu host -enable-kvm -smp 2 -m 4G -drive format=raw,file=./output/boot.img
+
 elif [[ "${1:-}" == "--docker" ]]; then
 	docker rmi -f mydistro-initramfs:latest || true
 	docker import ./output/initramfs.tar.gz mydistro-initramfs:latest
 	docker run --rm -it mydistro-initramfs:latest /bin/sh
+
 else
 	echo "Usage: $0 [--cli | --iso | --img | --docker]"
 fi
