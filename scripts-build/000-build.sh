c49b2e11a98499dcd4bb4d28372b2165da2dc52e
Faraz Fallahi <fffaraz@gmail.com>
2026-05-31 16:50:00 -0700

    fix

--- scripts-build/000-build.sh
+++ scripts-build/000-build.sh
@@ -18,13 +18,13 @@ export TEXINFO_XS=omit
 
 command -v git >/dev/null 2>&1 && git config --global --add safe.directory '*'
 
-./scripts/001-kernel.sh
 ./scripts/002-initramfs.sh
+./scripts/003-openssl.sh
+./scripts/001-kernel.sh
 ./scripts/030-nasm.sh
 ./scripts/005-syslinux.sh
 ./scripts/006-memtest86.sh
 ./scripts/039-libpng.sh
-./scripts/003-openssl.sh
 ./scripts/028-libxcrypt.sh
 ./scripts/040-freetype.sh
 ./scripts/007-microwindows.sh
