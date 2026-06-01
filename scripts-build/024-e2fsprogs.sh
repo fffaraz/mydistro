626bad2312f6d8bfb9fafb33798917d071bdf725
Faraz Fallahi <fffaraz@gmail.com>
2026-05-31 22:02:00 -0700

    Update 024-e2fsprogs.sh

--- scripts-build/024-e2fsprogs.sh
+++ scripts-build/024-e2fsprogs.sh
@@ -6,6 +6,14 @@ cd ./src/e2fsprogs
 mkdir -p build
 cd build
 
+# --disable-libblkid makes e2fsprogs link against the *external* libblkid
+# instead of its bundled copy. That external lib is the one util-linux (023)
+# just installed into $INITRAMFS_DIR, not anything in the build container, so
+# point the compiler/linker there. (In pass 2 the container is mydistro itself
+# and already ships libblkid in /usr, which is why this used to "just work".)
+export CPPFLAGS="-I$INITRAMFS_DIR/usr/include ${CPPFLAGS:-}"
+export LDFLAGS="-L$INITRAMFS_DIR/usr/lib ${LDFLAGS:-}"
+
 ../configure --prefix=/usr --sysconfdir=/etc --enable-elf-shlibs --disable-nls --disable-libblkid
 
 make
