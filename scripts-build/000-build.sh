b5cd9586ba2b54de58af52b84693a9d50c3b3c58
Faraz Fallahi <fffaraz@gmail.com>
2026-06-02 02:15:11 -0700

    Update 000-build.sh

--- scripts-build/000-build.sh
+++ scripts-build/000-build.sh
@@ -113,5 +113,5 @@ export PYTHONPATH="/usr/lib/python3-vendor${PYTHONPATH:+:$PYTHONPATH}"
 ./scripts/035-xorriso.sh
 ./scripts/013-mkisofs.sh
 # ./scripts/036-mtools.sh
-# ./scripts/037-dosfstools.sh
+./scripts/037-dosfstools.sh
 ./scripts/014-mk-boot-img.sh
