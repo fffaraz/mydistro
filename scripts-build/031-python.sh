6e5e2a4b719f9a0a95bf359bcae9c47ca3de2dd0
Faraz Fallahi <fffaraz@gmail.com>
2026-06-01 18:10:26 -0700

    Update 031-python.sh

--- scripts-build/031-python.sh
+++ scripts-build/031-python.sh
@@ -7,7 +7,11 @@ cd ./src/cpython
 	--prefix=/usr \
 	--enable-shared \
 	--without-ensurepip \
-	--without-static-libpython
+	--without-static-libpython \
+	--disable-test-modules
 
 make
 make install DESTDIR=$INITRAMFS_DIR
+
+find "$INITRAMFS_DIR" -name "__pycache__" -type d -exec rm -rf {} +
+find "$INITRAMFS_DIR" -name "*.pyc" -type f -delete
