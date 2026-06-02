c70219d147c0eb2f9a36c39ef8a2af505ebf9506
Faraz Fallahi <fffaraz@gmail.com>
2026-06-02 01:28:23 -0700

    Update 052-cpio.sh

--- scripts-build/052-cpio.sh
+++ scripts-build/052-cpio.sh
@@ -10,7 +10,11 @@ cd ./src/cpio
 
 ./bootstrap --skip-po --no-git --gnulib-srcdir=./gnulib
 
-FORCE_UNSAFE_CONFIGURE=1 ./configure --prefix=/usr --disable-nls
+# cpio declares `extern int (*xstat) ();` with an empty parameter list. Under
+# C23 (GCC 15's default) that means "takes no arguments", turning the stat/lstat
+# assignments and 2-arg calls into hard errors that -Wno-error can't downgrade.
+# Build against the older C17 semantics where `()` means "unspecified args".
+FORCE_UNSAFE_CONFIGURE=1 ./configure --prefix=/usr --disable-nls CFLAGS="$CFLAGS -std=gnu17"
 
 make
 make install DESTDIR=$INITRAMFS_DIR
